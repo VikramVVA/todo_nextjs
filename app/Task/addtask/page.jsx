d2bb5320cb090a90a9b670edbba4a9a84f93e91f
VikramVVA <vikramarekar00@gmail.com>
2023-10-18 12:59:03 +0530

    Changes done in task add page

--- app/Task/addtask/page.jsx
+++ app/Task/addtask/page.jsx
@@ -29,41 +29,40 @@ export default function Addtask() {
     };
     return (
         <>
-            <div className="flex flex-col items-center px-24 py-12">
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full px-3">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                            Enter Task Name
-                        </label>
-                        <div>
-                            <input
-                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                type="text"
-                                placeholder=""
-                                value={name}
-                                onChange={(e) => setTaskname(e.target.value)}
-                            />
-                            {error && (
-                                <span className="text-red-500">{error}</span>
-                            )}
-                        </div>
+            <div className="flex flex-col items-center">
+                <div className="w-1/2 px-8 py-8 mt-12 border border-gray-200 rounded">
+                    <div className="flex justify-center">
+                        <h1 className="uppercase text-gray-700 font-bold text-xl mb-8">Add Task</h1>
+                    </div>
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Enter Task Name
+                    </label>
+                    <div>
+                        <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            type="text"
+                            placeholder=""
+                            value={name}
+                            onChange={(e) => setTaskname(e.target.value)}
+                        />
+                        {error && <span className="text-red-500">{error}</span>}
+                    </div>
 
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                            Enter Task Description
-                        </label>
-                        <div>
-                            <input
-                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                type="text"
-                                placeholder=""
-                                value={description}
-                                onChange={(e) =>
-                                    setTaskDescription(e.target.value)
-                                }
-                            />
-                        </div>
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Enter Task Description
+                    </label>
+                    <div>
+                        <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            type="text"
+                            placeholder=""
+                            value={description}
+                            onChange={(e) => setTaskDescription(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex justify-end mt-8">
                         <button
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            className="w-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                             onClick={addTask}
                         >
                             Add
