1dbb3dc985fff7ec99c1ce62da27cedbf2a56f37
VikramVVA <vikramarekar00@gmail.com>
2023-10-18 15:51:58 +0530

    Small changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import Head from "next/head";
 
 export default function Mainpage() {
-    <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-    </Head>;
     return (
         <>
             <h1>This is a todo app </h1>
