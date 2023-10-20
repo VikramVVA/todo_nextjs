719f6ed82c37ffc1820ec6e2efa25995401eed57
VikramVVA <vikramarekar00@gmail.com>
2023-10-20 10:00:53 +0530

    Logo added in header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 export default function Header() {
     return (
         <>
-            <div className="bg-black text-white flex flex-row justify-between p-8 sticky top-0 z-10">
-                <div></div>
-                <div>
+            <div className="bg-black text-white flex flex-row justify-between p-4 sticky top-0 z-10">
+                <div className="p-2">
+                    <Image
+                        src="/favicon.ico"
+                        width={60}
+                        height={60}
+                        alt="Picture of the author"
+                    />
+                </div>
+                <div className="p-4">
                     <ul className="flex flex-row mx-4">
                         <li className="mx-2">
                             <Link href="/">Home</Link>
@@ -19,7 +27,7 @@ export default function Header() {
                         </li>
                     </ul>
                 </div>
-                <div>
+                <div className="p-4">
                     <button className="bg-blue-500 px-4 py-2 rounded-lg">
                         Logout
                     </button>
